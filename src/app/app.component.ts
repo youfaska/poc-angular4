967aec9d0de9214b0710d3db8d1b797271dec352
youfaska <youssef.oufaska@aytos.es>
2017-05-26 14:11:16 +0200

    test commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My First Angular App';
+  title = 'Asset&Fleet, Welcome to my world: ANGULAR 4 - FRAN';
 }
